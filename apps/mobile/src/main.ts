cfdb5223f011703e5e458b4c4c903d575006cbfc
China-quanda <877880098@qq.com>
2024-05-18 23:49:39 +0800

    add swagger

--- apps/mobile/src/main.ts
+++ apps/mobile/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { MobileModule } from './mobile.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import { join } from 'node:path';
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
   // app.setBaseViewsDir(join(__dirname, '../', 'views'));
   app.setBaseViewsDir('views');
   app.setViewEngine('ejs');
+
+  const config = new DocumentBuilder()
+    .setTitle('移动端api文档')
+    .setDescription('这是移动端的api文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, document);
+
   await app.listen(3002);
+  console.log('http://localhost:3002/document');
 }
 bootstrap();

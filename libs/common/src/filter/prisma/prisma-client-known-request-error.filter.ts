c940602ff25dea395763c026578bd4a6d6b7e17b
China-quanda <877880098@qq.com>
2024-05-19 13:28:23 +0800

    edit

--- libs/common/src/filter/prisma/prisma-client-known-request-error.filter.ts
+++ libs/common/src/filter/prisma/prisma-client-known-request-error.filter.ts
@@ -10,8 +10,14 @@ export class PrismaClientKnownRequestErrorFilter extends BaseExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const message = exception.message.replace(/\n/g, '');
-    if (exception.code === 'P2002' || exception.code === 'P2025') {
+    if (exception.code === 'P2002') {
       const status = HttpStatus.CONFLICT;
+      response.status(status).json({
+        statusCode: status,
+        message: message,
+      });
+    } else if (exception.code === 'P2025') {
+      const status = HttpStatus.NOT_FOUND;
       response.status(status).json({
         statusCode: status,
         message: message,

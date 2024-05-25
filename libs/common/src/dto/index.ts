59f5651242cb6852ce77ca00ce674364d4b3769e
China-quanda <877880098@qq.com>
2024-05-25 21:57:06 +0800

    edit

--- libs/common/src/dto/index.ts
+++ libs/common/src/dto/index.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Min } from 'class-validator';
 export class BaseQueryDto {
-  @IsInt()
   @Min(1)
+  @IsInt()
+  @Type(() => Number)
+  @IsOptional()
   @ApiProperty({ description: '页码', default: 1 })
   pageNum?: number;
-  @IsInt()
+
   @Min(1)
+  @IsInt()
+  @Type(() => Number)
+  @IsOptional()
   @ApiProperty({ description: '每页条数', default: 10 })
   pageSize?: number;
 }

c5ce0f45995bf990455790276600b343b728798b
China-quanda <877880098@qq.com>
2024-06-11 14:19:28 +0800

    权限验证白名单

--- apps/admin/src/auth/jwt-auth.guard.ts
+++ apps/admin/src/auth/jwt-auth.guard.ts
@@ -4,16 +4,33 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
+import { pathToRegexp } from 'path-to-regexp';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  // 白名单
+  private globalWhiteList = [];
   constructor(private reflector: Reflector) {
     super();
+    // this.globalWhiteList = [
+    //   { path: '/captchaImage', method: 'GET' },
+    //   { path: '/register', method: 'POST' },
+    //   { path: '/login', method: 'POST' },
+    //   { path: '/logout', method: 'POST' },
+    //   { path: '/perm/{id}', method: 'GET' },
+    //   { path: '/upload', method: 'POST' },
+    // ];
   }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // 如果在白名单直接跳过
+    // const isInWhiteList = this.checkWhiteList(ctx);
+    // if (isInWhiteList) {
+    //   return true;
+    // }
+
     // 如果是公开接口，则直接放行 不需认证
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
@@ -23,5 +40,24 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
     return super.canActivate(context);
+  }
+
+  /**
+   * 检查接口是否在白名单内
+   * @param ctx
+   * @returns
+   */
+  checkWhiteList(ctx: ExecutionContext): boolean {
+    const req = ctx.switchToHttp().getRequest();
+    const i = this.globalWhiteList.findIndex((route) => {
+      // 请求方法类型相同
+      if (req.method.toUpperCase() === route.method.toUpperCase()) {
+        // 对比 url
+        return !!pathToRegexp(route.path).exec(req.url);
+      }
+      return false;
+    });
+    // 在白名单内 则 进行下一步， i === -1 ，则不在白名单，需要 比对是否有当前接口权限
+    return i > -1;
   }
 }

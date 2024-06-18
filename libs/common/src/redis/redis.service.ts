9e5adc3a3ee16965e93064336780650aefb5a17e
China-quanda <877880098@qq.com>
2024-06-18 19:16:57 +0800

    edit RedisService

--- libs/common/src/redis/redis.service.ts
+++ libs/common/src/redis/redis.service.ts
@@ -1,5 +1,6 @@
 // redis.service.ts
 import { Injectable } from '@nestjs/common';
+// https://github.com/redis/ioredis
 import Redis from 'ioredis';
 import { ConfigService } from '@nestjs/config';
 import { RedisConfig } from '../config';
@@ -21,5 +22,33 @@ export class RedisService {
 
   getValue(key: string) {
     return this.redisClient.get(key);
+  }
+
+  // 存入值，且设置过期时间：
+  setWithExpiry(key: string, value: string, time: number) {
+    return this.redisClient.setex(key, time, value);
+  }
+
+  // 删除数据
+  deleteKey(key: string) {
+    return this.redisClient.del(key);
+  }
+
+  // 使用哈希表（Hash）
+  // 在哈希表（Hash）相关的方法中，field这是哈希表中的字段名称。
+
+  //设置key中filed字段的数据value
+  setHashField(key: string, field: string, value: string) {
+    return this.redisClient.hset(key, field, value);
+  }
+
+  //获取key中的field字段数据
+  getHashField(key: string, field: string) {
+    return this.redisClient.hget(key, field);
+  }
+
+  //获取key中所有数据
+  getAllHashFields(key: string) {
+    return this.redisClient.hgetall(key);
   }
 }
